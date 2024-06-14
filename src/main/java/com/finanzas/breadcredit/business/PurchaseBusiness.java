92988df6b134ec9f62ae40dcb69cecea21d8f1f9
Fernando Schilder <u20201b814@upc.edu.pe>
2024-06-14 11:29:32 -0500

    feat: Añadir manejo de excepciones de Purchase

--- src/main/java/com/finanzas/breadcredit/business/PurchaseBusiness.java
+++ src/main/java/com/finanzas/breadcredit/business/PurchaseBusiness.java
@@ -1,6 +1,7 @@
 package com.finanzas.breadcredit.business;
 
 import com.finanzas.breadcredit.entity.Purchase;
+import com.finanzas.breadcredit.exception.ResourceNotFoundException;
 import com.finanzas.breadcredit.repository.CreditaccountRepository;
 import com.finanzas.breadcredit.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,47 +13,52 @@ import java.util.List;
 @Service
 public class PurchaseBusiness {
 
-    @Autowired
-    private PurchaseRepository purchaseRepository;
+
+    private final PurchaseRepository purchaseRepository;
+
+
+    private final CreditaccountRepository creditaccountRepository;
 
     @Autowired
-    private CreditaccountRepository creditaccountRepository;
+    public PurchaseBusiness(PurchaseRepository purchaseRepository, CreditaccountRepository creditaccountRepository) {
+        this.purchaseRepository = purchaseRepository;
+        this.creditaccountRepository = creditaccountRepository;
+    }
 
-    public Purchase insertPurchase(Purchase purchase) throws Exception{
+    public Purchase insertPurchase(Purchase purchase) {
         purchase.setId(null);
         purchase.setTime(Instant.now());
         return purchaseRepository.save(purchase);
     }
 
-    public Purchase getPurchaseById(Integer id) throws Exception{
-        return purchaseRepository.findById(id).orElseThrow(() -> new Exception("Purchase not found"));
+    public Purchase getPurchaseById(Integer id) throws ResourceNotFoundException {
+        return purchaseRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Purchase { id=" + id + " } not found"));
     }
 
-    public List<Purchase> listPurchases() throws Exception{
+    public List<Purchase> listPurchases() throws ResourceNotFoundException {
         List<Purchase> purchaseList = purchaseRepository.findAll();
-        if(purchaseList.isEmpty()){
-            throw new Exception("No purchases found");
+        if (purchaseList.isEmpty()) {
+            throw new ResourceNotFoundException("No purchases found");
         }
         return purchaseList;
     }
 
-    public Purchase updatePurchase(Integer id, Purchase purchase) throws Exception{
+    public Purchase updatePurchase(Integer id, Purchase purchase) throws ResourceNotFoundException {
         purchase.setId(id);
-        if(!creditaccountRepository.existsById(purchase.getCreditaccount().getId())){
-            throw new Exception("Credit account not found");
+        if (!creditaccountRepository.existsById(purchase.getCreditaccount().getId())) {
+            throw new ResourceNotFoundException("Credit account { id=" + purchase.getCreditaccount().getId() + " }  not found");
         }
-        if(!purchaseRepository.existsById(purchase.getId())){
-            System.out.println(purchase.getId());
-            throw new Exception("Purchase not found");
+        if (!purchaseRepository.existsById(purchase.getId())) {
+            throw new ResourceNotFoundException("Purchase { id=" + id + " } not found");
         }
         return purchaseRepository.save(purchase);
     }
 
-    public void deletePurchase(Integer id) throws Exception{
-        if(!purchaseRepository.existsById(id)){
-            throw new Exception("Purchase not found");
+    public void deletePurchase(Integer id) throws ResourceNotFoundException {
+        if (!purchaseRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Purchase not found");
         }
         purchaseRepository.deleteById(id);
     }
-
 }
--- src/main/java/com/finanzas/breadcredit/controller/PurchaseController.java
+++ src/main/java/com/finanzas/breadcredit/controller/PurchaseController.java
@@ -7,73 +7,55 @@ import com.finanzas.breadcredit.entity.Purchase;
 import com.finanzas.breadcredit.utility.UtilityDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/purchase")
 public class PurchaseController {
+
+    private final PurchaseBusiness purchaseBusiness;
+
     @Autowired
-    private PurchaseBusiness purchaseBusiness;
+    public PurchaseController(PurchaseBusiness purchaseBusiness) {
+        this.purchaseBusiness = purchaseBusiness;
+    }
 
     @PostMapping("")
-    public ResponseEntity<PurchaseDtoData> purchase(@RequestBody PurchaseDtoInsert purchaseDtoInsert) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public PurchaseDtoData insertPurchase(@RequestBody PurchaseDtoInsert purchaseDtoInsert) {
         Purchase purchase = UtilityDto.convertTo(purchaseDtoInsert, Purchase.class);
-        try {
-            purchase = purchaseBusiness.insertPurchase(purchase);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        }
-        PurchaseDtoData purchaseDtoData = UtilityDto.convertTo(purchase, PurchaseDtoData.class);
-        return new ResponseEntity<>(purchaseDtoData, HttpStatus.CREATED);
+        purchase = purchaseBusiness.insertPurchase(purchase);
+        return UtilityDto.convertTo(purchase, PurchaseDtoData.class);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PurchaseDtoData> getPurchaseBy(@PathVariable Integer id) {
-        Purchase purchase;
-        try {
-            purchase = purchaseBusiness.getPurchaseById(id);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-        PurchaseDtoData purchaseDtoData = UtilityDto.convertTo(purchase, PurchaseDtoData.class);
-        return new ResponseEntity<>(purchaseDtoData, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public PurchaseDtoData getPurchaseById(@PathVariable Integer id) {
+        Purchase purchase = purchaseBusiness.getPurchaseById(id);
+        return UtilityDto.convertTo(purchase, PurchaseDtoData.class);
     }
 
     @GetMapping("")
-    public ResponseEntity<List<PurchaseDtoData>> listPurchases() {
-        List<Purchase> purchaseList;
-        try {
-            purchaseList = purchaseBusiness.listPurchases();
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-        List<PurchaseDtoData> purchaseDtoDataList = UtilityDto.convertToList(purchaseList, PurchaseDtoData.class);
-        return new ResponseEntity<>(purchaseDtoDataList, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public List<PurchaseDtoData> listPurchases() {
+        List<Purchase> purchaseList = purchaseBusiness.listPurchases();
+        return UtilityDto.convertToList(purchaseList, PurchaseDtoData.class);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PurchaseDtoData> updatePurchase(@PathVariable Integer id, @RequestBody PurchaseDtoInsert purchaseDtoInsert) {
+    @ResponseStatus(HttpStatus.OK)
+    public PurchaseDtoData updatePurchase(@PathVariable Integer id, @RequestBody PurchaseDtoInsert purchaseDtoInsert) {
         Purchase purchase = UtilityDto.convertTo(purchaseDtoInsert, Purchase.class);
-        try {
-            purchase = purchaseBusiness.updatePurchase(id, purchase);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
-        PurchaseDtoData purchaseDtoData = UtilityDto.convertTo(purchase, PurchaseDtoData.class);
-        return new ResponseEntity<>(purchaseDtoData, HttpStatus.OK);
+        purchase = purchaseBusiness.updatePurchase(id, purchase);
+        return UtilityDto.convertTo(purchase, PurchaseDtoData.class);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePurchase(@PathVariable Integer id) {
-        try {
-            purchaseBusiness.deletePurchase(id);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Void deletePurchase(@PathVariable Integer id) {
+        purchaseBusiness.deletePurchase(id);
+        return null;
     }
 }
