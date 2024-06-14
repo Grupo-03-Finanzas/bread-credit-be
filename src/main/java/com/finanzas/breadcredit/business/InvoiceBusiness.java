2e3d15188453ca9601fe2e8d5ed31fee490509d1
Fernando Schilder <u20201b814@upc.edu.pe>
2024-06-14 13:09:46 -0500

    feat: Añadir manejo de excepciones de Invoice

--- src/main/java/com/finanzas/breadcredit/business/InvoiceBusiness.java
+++ src/main/java/com/finanzas/breadcredit/business/InvoiceBusiness.java
@@ -1,7 +1,7 @@
 package com.finanzas.breadcredit.business;
 
-
 import com.finanzas.breadcredit.entity.Invoice;
+import com.finanzas.breadcredit.exception.ResourceNotFoundException;
 import com.finanzas.breadcredit.repository.InvoiceRepository;
 import com.finanzas.breadcredit.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,46 +13,43 @@ import java.util.List;
 @Service
 public class InvoiceBusiness {
 
-    @Autowired
-    private InvoiceRepository invoiceRepository;
+    private final InvoiceRepository invoiceRepository;
 
     @Autowired
-    private PaymentRepository paymentRepository;
+    public InvoiceBusiness(InvoiceRepository invoiceRepository, PaymentRepository paymentRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
 
-    public Invoice insertInvoice(Invoice invoice) throws Exception {
+    public Invoice getInvoiceById(Integer id) throws ResourceNotFoundException {
+        return invoiceRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Invoice { id=" + id + " } not found"));
+    }
+
+    public List<Invoice> listInvoices() throws ResourceNotFoundException {
+        List<Invoice> invoiceList = invoiceRepository.findAll();
+        if (invoiceList.isEmpty()) {
+            throw new ResourceNotFoundException("Invoices not found");
+        }
+        return invoiceList;
+    }
+
+    public Invoice insertInvoice(Invoice invoice) {
         invoice.setId(null);
         invoice.setPayment(null);
         return invoiceRepository.save(invoice);
     }
 
-    public Invoice getInvoiceById(Integer id) throws Exception {
-        return invoiceRepository.findById(id).orElseThrow(() -> new Exception("Invoice not found"));
-    }
-
-    public List<Invoice> listInvoices() throws Exception {
-        List<Invoice> invoiceList = invoiceRepository.findAll();
-
-        //if (invoiceList.isEmpty()) {
-        //    throw new Exception("No invoices found");
-        //}
-
-        return invoiceList;
-    }
-
     @Transactional
-    public Invoice updateInvoice(Integer id, Invoice invoice) throws Exception {
+    public Invoice updateInvoice(Integer id, Invoice invoice) throws ResourceNotFoundException {
         invoice.setId(id);
-
         if (!invoiceRepository.existsById(id)) {
-            throw new Exception("Invoice not found");
+            throw new ResourceNotFoundException("Invoice { id=" + id + " } not found");
         }
-
         return invoiceRepository.save(invoice);
     }
 
     @Transactional
-    public void deleteInvoice(Integer id) throws Exception {
-        Invoice invoice = invoiceRepository.findById(id).orElseThrow(() -> new Exception("Invoice not found"));
+    public void deleteInvoice(Integer id) throws ResourceNotFoundException {
+        Invoice invoice = invoiceRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Invoice { id=" + id + " } not found"));
         invoiceRepository.delete(invoice);
     }
 }
--- src/main/java/com/finanzas/breadcredit/controller/InvoiceController.java
+++ src/main/java/com/finanzas/breadcredit/controller/InvoiceController.java
@@ -4,76 +4,59 @@ import com.finanzas.breadcredit.business.InvoiceBusiness;
 import com.finanzas.breadcredit.dto.invoice.InvoiceDtoData;
 import com.finanzas.breadcredit.dto.invoice.InvoiceDtoInsert;
 import com.finanzas.breadcredit.entity.Invoice;
+import com.finanzas.breadcredit.exception.ResourceNotFoundException;
 import com.finanzas.breadcredit.utility.UtilityDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/invoice")
 public class InvoiceController {
-    @Autowired
-    private InvoiceBusiness invoiceBusiness;
 
-    @PostMapping("")
-    public ResponseEntity<InvoiceDtoData> createInvoice(@RequestBody InvoiceDtoInsert invoiceDtoInsert) {
-        Invoice invoice = UtilityDto.convertTo(invoiceDtoInsert, Invoice.class);
-        try {
-            invoice = invoiceBusiness.insertInvoice(invoice);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        }
-        InvoiceDtoData invoiceDtoData = UtilityDto.convertTo(invoice, InvoiceDtoData.class);
-        return new ResponseEntity<>(invoiceDtoData, HttpStatus.CREATED);
+    private final InvoiceBusiness invoiceBusiness;
+
+    @Autowired
+    public InvoiceController(InvoiceBusiness invoiceBusiness) {
+        this.invoiceBusiness = invoiceBusiness;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<InvoiceDtoData> getInvoiceById(@PathVariable Integer id) {
-        Invoice invoice;
-        try {
-            invoice = invoiceBusiness.getInvoiceById(id);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-        InvoiceDtoData invoiceDtoData = UtilityDto.convertTo(invoice, InvoiceDtoData.class);
-        return new ResponseEntity<>(invoiceDtoData, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public InvoiceDtoData getInvoiceById(@PathVariable Integer id) throws ResourceNotFoundException {
+        Invoice invoice = invoiceBusiness.getInvoiceById(id);
+        return UtilityDto.convertTo(invoice, InvoiceDtoData.class);
     }
 
     @GetMapping("")
-    public ResponseEntity<List<InvoiceDtoData>> listInvoices() {
-        List<Invoice> invoiceList;
-        try {
-            invoiceList = invoiceBusiness.listInvoices();
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-        List<InvoiceDtoData> invoiceDtoDataList = UtilityDto.convertToList(invoiceList, InvoiceDtoData.class);
-        return new ResponseEntity<>(invoiceDtoDataList, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public List<InvoiceDtoData> listInvoices() throws ResourceNotFoundException {
+        List<Invoice> invoiceList = invoiceBusiness.listInvoices();
+        return UtilityDto.convertToList(invoiceList, InvoiceDtoData.class);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public InvoiceDtoData insertInvoice(@RequestBody InvoiceDtoInsert invoiceDtoInsert) {
+        Invoice invoice = UtilityDto.convertTo(invoiceDtoInsert, Invoice.class);
+        invoice = invoiceBusiness.insertInvoice(invoice);
+        return UtilityDto.convertTo(invoice, InvoiceDtoData.class);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<InvoiceDtoData> updateInvoice(@PathVariable Integer id, @RequestBody InvoiceDtoInsert invoiceDtoInsert) {
+    @ResponseStatus(HttpStatus.OK)
+    public InvoiceDtoData updateInvoice(@PathVariable Integer id, @RequestBody InvoiceDtoInsert invoiceDtoInsert) throws ResourceNotFoundException {
         Invoice invoice = UtilityDto.convertTo(invoiceDtoInsert, Invoice.class);
-        try {
-            invoice = invoiceBusiness.updateInvoice(id, invoice);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
-        InvoiceDtoData invoiceDtoData = UtilityDto.convertTo(invoice, InvoiceDtoData.class);
-        return new ResponseEntity<>(invoiceDtoData, HttpStatus.OK);
+        invoice = invoiceBusiness.updateInvoice(id, invoice);
+        return UtilityDto.convertTo(invoice, InvoiceDtoData.class);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteInvoice(@PathVariable Integer id) {
-        try {
-            invoiceBusiness.deleteInvoice(id);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Void deleteInvoice(@PathVariable Integer id) throws ResourceNotFoundException {
+        invoiceBusiness.deleteInvoice(id);
+        return null;
     }
 }
