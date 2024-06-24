48c77cfde80c659502021e9f75eaf0c6b35211bb
Fernando Schilder <u20201b814@upc.edu.pe>
2024-06-23 20:51:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo docker rm $(sudo docker ps -aq -f status=exited)
+sudo docker image prune -f
 sudo docker volume rm $(sudo docker volume ls -q)
 git pull https://github.com/Grupo-03-Finanzas/bread-credit-be.git prod-azure
 sudo docker compose up --build
